2dd60bc3877cef1abc8df68883cc40391b7213fd
Duncan <drdunc@gmail.com>
2017-01-10 23:53:41 +0000

    term 1 summarising
    moved all the content for term one into sub folder to make it quicker to load home page. also moved list of clubbers to a seperate page to make it easier to view on smaller screens.

---
+++ clubmenulist.js
@@ -0,0 +1,26 @@
+document.write('\
+\
+<a href="https://scratch.mit.edu/users/MrPinkShep">MrPinkShep</a><br>\
+<a href="https://scratch.mit.edu/users/potato081">potato081</a><br>\
+<a href="https://scratch.mit.edu/users/mimi24798">mimi24798</a><br>\
+<a href="https://scratch.mit.edu/users/creativego1H2">creativego1H2</a><br>\
+<a href="https://scratch.mit.edu/users/crazydann">crazydann</a><br>\
+<a href="https://scratch.mit.edu/users/insert_my_name_here">insert_my_name_here</a><br>\
+<a href="https://scratch.mit.edu/users/flubhub">flubhub</a><br>\
+<a href="https://scratch.mit.edu/users/forevergirlzxox123">forevergirlzxox123</a><br>\
+<a href="https://scratch.mit.edu/users/Paco0409">Paco0409</a><br>\
+<a href="https://scratch.mit.edu/users/icecream925">icecream925</a><br>\
+<a href="https://scratch.mit.edu/users/candyfloss7000">candyfloss7000</a><br>\
+<a href="https://scratch.mit.edu/users/lolmooltheblack2">lolmooltheblack2</a><br>\
+<a href="https://scratch.mit.edu/users/loladoodah123">loladoodah123</a><br>\
+<a href="https://scratch.mit.edu/users/kaker">kaker</a><br>\
+<a href="https://scratch.mit.edu/users/lolscratch2006">lolscratch2006</a><br>\
+<a href="https://scratch.mit.edu/users/katydoodah">katydoodah</a><br>\
+<a href="https://scratch.mit.edu/users/webgirl22">webgirl22</a><br>\
+<a href="https://scratch.mit.edu/users/cakebin">cakebin</a><br>\
+<a href="https://scratch.mit.edu/users/jamievardy2015">jamievardy2015</a><br>\
+<a href="https://scratch.mit.edu/users/lolmooltheblack">lolmooltheblack</a><br>\
+<a href="https://scratch.mit.edu/users/mrawesomescratcher">MrAwesomeScratcher (The Amazing one!)</a><br>\
+<a href="https://scratch.mit.edu/users/jtw123">jtw123</a><br>\
+\
+');
