7a15deccc52c13b1d47b907a87d92b6020ef764f
FlamingFireball64 <flamingfireball64@gmail.com>
2017-09-22 16:27:29 +0100

    Update script.js

--- members/script.js
+++ members/script.js
@@ -3,5 +3,6 @@ function listMembers() {
   var counter = 0;
   while (counter < members.length) {
     document.getElementById("memberList").innerHTML = document.getElementById("memberList").innerHTML + "<br /><a href='https://scratch.mit.edu/users/" + members[counter] + "' target='_blank'>" + members[counter] + "</a>";
+    counter = counter + 1;
   }
 }
