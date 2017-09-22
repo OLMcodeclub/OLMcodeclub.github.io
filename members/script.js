5df94b3a5e394c204c9b749d087c5eb10280d858
FlamingFireball64 <flamingfireball64@gmail.com>
2017-09-22 16:33:15 +0100

    Update script.js

--- members/script.js
+++ members/script.js
@@ -1,5 +1,5 @@
 function listMembers() {
-  var members = ["Paco0409", "Mimi24798", "Kaker", "Potato081", "Crazydann", "Pulif", "Biancaio29", "PurpleNinja3"];
+  var members = ["Paco0409", "Mimi24798", "Kaker", "Potato081", "Crazydann", "Pulif", "Biancaio29", "PurpleNinja3", "GrandDiamond67"];
   var counter = 0;
   while (counter < members.length) {
     document.getElementById("memberList").innerHTML = document.getElementById("memberList").innerHTML + "<br /><a href='https://scratch.mit.edu/users/" + members[counter] + "' target='_blank'>" + members[counter] + "</a>";
