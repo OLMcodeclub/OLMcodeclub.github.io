6f91827e0e3521425f2c420aee396d231eeaf476
Duncan <drdunc@gmail.com>
2017-11-24 17:18:48 +0000

    updates

--- docs/astro.py
+++ docs/astro.py
@@ -7,299 +7,301 @@ from random import randint
 sense = SenseHat()
 sense.set_rotation(270)
 
-# MDF
+while 1:
+	# MDF
 
-bc = (0, 0, 205)
-tc = (255,165,0)
-c = 0
+	bc = (0, 0, 205)
+	tc = (255,165,0)
+	c = 0
 
-sense.clear(bc)
+	sense.clear(bc)
 
-while c < 280:
-  sense.set_pixel(randint(0,7), randint(0,7), randint(0,255), randint(0,255), randint(0,255))
-  c = c + 1
-  sleep(0.03)
+	while c < 280:
+	  sense.set_pixel(randint(0,7), randint(0,7), randint(0,255), randint(0,255), randint(0,255))
+	  c = c + 1
+	  sleep(0.03)
 
-t1 = round( sense.get_temperature(), 1 )
-t2 = round( sense.get_temperature_from_pressure(), 3)
-sense.show_message( str(t1) + " C or " + str(t2) + " C" , text_colour=tc, back_colour=bc)
+	t1 = round( sense.get_temperature(), 1 )
+	t2 = round( sense.get_temperature_from_pressure(), 3)
+	sense.show_message( str(t1) + " C or " + str(t2) + " C" , text_colour=tc, back_colour=bc)
 
-c = 0
-while c < 280:
-  sense.set_pixel(randint(0,7), randint(0,7), randint(0,255), randint(0,255), randint(0,255))
-  c = c + 1
-  sleep(0.03)
+	c = 0
+	while c < 280:
+	  sense.set_pixel(randint(0,7), randint(0,7), randint(0,255), randint(0,255), randint(0,255))
+	  c = c + 1
+	  sleep(0.03)
 
-#constance and william
-sense.show_message("Hello how boring is it in space?")
+	#constance and william
+	sense.show_message("Hello how boring is it in space?")
 
-w = (32,178,170)
+	w = (32,178,170)
 
-b = (0, 0, 0)
+	b = (0, 0, 0)
 
-picture = [
-    b, b, b, b, b, b, b, b,
-    b, b, w, w, b, w, w, b,
-    b, b, w, w, b, w, w, b,
-    b, w, b, b, b, b, b, w,
-    b, w, w, w, w, w, w, w,
-    b, b, b, b, b, b, b, b,
-    b, b, b, b, b, b, b, b,
-    b, b, b, b, b, b, b, b
-]
-sense.set_pixels(picture)
+	picture = [
+	    b, b, b, b, b, b, b, b,
+	    b, b, w, w, b, w, w, b,
+	    b, b, w, w, b, w, w, b,
+	    b, w, b, b, b, b, b, w,
+	    b, w, w, w, w, w, w, w,
+	    b, b, b, b, b, b, b, b,
+	    b, b, b, b, b, b, b, b,
+	    b, b, b, b, b, b, b, b
+	]
+	sense.set_pixels(picture)
 
-temp = round( sense.get_temperature(), 1 )
+	temp = round( sense.get_temperature(), 1 )
 
-sense.show_message( str(temp) )
+	sense.show_message( str(temp) )
 
-#oscar and juliette
+	#oscar and juliette
 
-bc = (17, 173, 48)
-tc = (173, 17, 17)
-c = 0
+	bc = (17, 173, 48)
+	tc = (173, 17, 17)
+	c = 0
 
-sense.clear(bc)
+	sense.clear(bc)
 
-while c < 280:
-  sense.set_pixel(randint(0,7), randint(0,7), randint(0,255), randint(0,255), randint(0,255))
-  c = c + 1
-  sleep(0.03)
+	while c < 280:
+	  sense.set_pixel(randint(0,7), randint(0,7), randint(0,255), randint(0,255), randint(0,255))
+	  c = c + 1
+	  sleep(0.03)
 
-t1 = round( sense.get_temperature(), 1 )
-t2 = round( sense.get_temperature_from_pressure(), 3)
-sense.show_message( str(t1) + " C or " + str(t2) + " C" , text_colour=tc, back_colour=bc)
+	t1 = round( sense.get_temperature(), 1 )
+	t2 = round( sense.get_temperature_from_pressure(), 3)
+	sense.show_message( str(t1) + " C or " + str(t2) + " C" , text_colour=tc, back_colour=bc)
 
-c = 0
-while c < 280:
-  sense.set_pixel(randint(0,7), randint(0,7), randint(0,255), randint(0,255), randint(0,255))
-  c = c + 1
-  sleep(0.03)
+	c = 0
+	while c < 280:
+	  sense.set_pixel(randint(0,7), randint(0,7), randint(0,255), randint(0,255), randint(0,255))
+	  c = c + 1
+	  sleep(0.03)
 
-# charlotte and grace
+	# charlotte and grace
 
-temp = round( sense.get_temperature(), 1 )
-sense.show_message( str(temp) )
-red = (255,0,0)
-sense.show_message("Greetings from earth" ,text_colour=red, scroll_speed=0.05)
-w = (255, 255, 255)
-b = (0, 0, 0)
-picture = [
-    b, b, b, b,b, b, b, b,
-    b, w, w, b, b, w, w, b,
-    b, w, w, b, b, w, w, b,
-    b, b, b, b, b, b, b, b,
-    w, b, b, b, b, b, b, w,
-    w, w, w, w, w, w, w, w,
-    b, w, w, w, w, w, w, b,
-    b, b, b, b, b, b, b, b
-]
-sense.set_pixels(picture)
+	temp = round( sense.get_temperature(), 1 )
+	sense.show_message( str(temp) )
+	red = (255,0,0)
+	sense.show_message("Greetings from earth" ,text_colour=red, scroll_speed=0.05)
+	w = (255, 255, 255)
+	b = (0, 0, 0)
+	picture = [
+	    b, b, b, b,b, b, b, b,
+	    b, w, w, b, b, w, w, b,
+	    b, w, w, b, b, w, w, b,
+	    b, b, b, b, b, b, b, b,
+	    w, b, b, b, b, b, b, w,
+	    w, w, w, w, w, w, w, w,
+	    b, w, w, w, w, w, w, b,
+	    b, b, b, b, b, b, b, b
+	]
+	sense.set_pixels(picture)
 
-# grace and charlotte
+	# grace and charlotte
 
-d = (139, 0, 139)
-sense.show_message("OLM are in space!", text_colour=d, scroll_speed= 0.06 )
-a = (0, 250, 154)
-b = (255, 255, 0)
-c = (238, 154, 0)
-de = (255, 255, 255)
-e = (0, 0, 0)
-picture = [
- e, e, e, e, e, e, e, e,
- e, de, a, e, e, de, a, e,
- e, a, a, e, e, a, a, e,
- e, e, e, e, e, e, e, e,
- c, c, e, e, e, e, c, c,
- e, c, c, c, c, c, c, e,
- e, e, c, c, c, c, e, e,
- e, e, e, e, e, e, e, e,
- ]
-sense.set_pixels(picture)
-sleep(3)
+	d = (139, 0, 139)
+	sense.show_message("OLM are in space!", text_colour=d, scroll_speed= 0.06 )
+	a = (0, 250, 154)
+	b = (255, 255, 0)
+	c = (238, 154, 0)
+	de = (255, 255, 255)
+	e = (0, 0, 0)
+	picture = [
+	 e, e, e, e, e, e, e, e,
+	 e, de, a, e, e, de, a, e,
+	 e, a, a, e, e, a, a, e,
+	 e, e, e, e, e, e, e, e,
+	 c, c, e, e, e, e, c, c,
+	 e, c, c, c, c, c, c, e,
+	 e, e, c, c, c, c, e, e,
+	 e, e, e, e, e, e, e, e,
+	 ]
+	sense.set_pixels(picture)
+	sleep(3)
 
-r = (255, 255, 255)
-x = (255 ,14 ,6)
-temp = sense.get_temperature()
-temp = round( sense.get_temperature(), 1 )
-sense.show_message( str(temp) )
-sense.show_message( "It is " + str(temp) + " degrees", text_colour=r, back_colour=x )
+	r = (255, 255, 255)
+	x = (255 ,14 ,6)
+	temp = sense.get_temperature()
+	temp = round( sense.get_temperature(), 1 )
+	sense.show_message( str(temp) )
+	sense.show_message( "It is " + str(temp) + " degrees", text_colour=r, back_colour=x )
 
-# pilar
+	# pilar
 
-sense.show_message("Astro Pi")
-b=(0,0,0)
-w=(44,63,94)
-picture = [
-    b, b, w, w, w, w, b, b,
-    b, w, b, b, b, b, w, b,
-    b, w, b, w, w, b, w, b,
-    b, w, b, b, b, b, w, b,
-    b, b, w, w, w, w, b, b,
-    b, b, w, w, w, w, b, b,
-    b, w, w, w, w, w, w, b,
-    b, w, w, w, w, w, w, b
-]
+	sense.show_message("Astro Pi")
+	b=(0,0,0)
+	w=(44,63,94)
+	picture = [
+	    b, b, w, w, w, w, b, b,
+	    b, w, b, b, b, b, w, b,
+	    b, w, b, w, w, b, w, b,
+	    b, w, b, b, b, b, w, b,
+	    b, b, w, w, w, w, b, b,
+	    b, b, w, w, w, w, b, b,
+	    b, w, w, w, w, w, w, b,
+	    b, w, w, w, w, w, w, b
+	]
 
-sense.set_pixels(picture)
-sleep(2)
+	sense.set_pixels(picture)
+	sleep(2)
 
-sense.show_message("the current temp is",scroll_speed=0.05)
-temp = round( sense.get_temperature(), 1 )
-sense.show_message("it is"+ str(temp) )
-w = (255, 255, 255)
-y = (255, 255, 0)
-g = (0, 255, 0)
-b = (0, 0, 0)
-hot = [
-  b, b, b, b, b, y, y, b,
-  b, b, b, b, y, y, y, y,
-  b, b, b, b, b, y, y, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  g, g, g, g, g, g, g, g,
-  g, g, g, g, g, g, g, g
-]
-
-
-cold = [
-  b, b, w, b, b, b, w, b,
-  b, b, b, b, b, w, b, b,
-  b, w, b, b, b, b, b, w,
-  b, b, b, b, w, b, b, b,
-  w, b, b, w, b, b, w, b,
-  b, b, b, b, b, b, b, b,
-  w, w, w, w, w, w, w, w,
-  w, w, w, w, w, w, w, w
-]
-temp = sense.get_temperature()
-if temp >= 20:
-    sense.set_pixels(hot)
-else:
-    sense.set_pixels(cold)
-    
-bc = (0, 0, 205)
-tc = (255,165,0)
-c = 0
-
-# william and storm
-
-sense.show_message("hello I like science!!!!")
-b = (0,0,0)
-y = (255,255,0)
-r = (255,0,0)
+	sense.show_message("the current temp is",scroll_speed=0.05)
+	temp = round( sense.get_temperature(), 1 )
+	sense.show_message("it is"+ str(temp) )
+	w = (255, 255, 255)
+	y = (255, 255, 0)
+	g = (0, 255, 0)
+	b = (0, 0, 0)
+	hot = [
+	  b, b, b, b, b, y, y, b,
+	  b, b, b, b, y, y, y, y,
+	  b, b, b, b, b, y, y, b,
+	  b, b, b, b, b, b, b, b,
+	  b, b, b, b, b, b, b, b,
+	  b, b, b, b, b, b, b, b,
+	  g, g, g, g, g, g, g, g,
+	  g, g, g, g, g, g, g, g
+	]
 
 
+	cold = [
+	  b, b, w, b, b, b, w, b,
+	  b, b, b, b, b, w, b, b,
+	  b, w, b, b, b, b, b, w,
+	  b, b, b, b, w, b, b, b,
+	  w, b, b, w, b, b, w, b,
+	  b, b, b, b, b, b, b, b,
+	  w, w, w, w, w, w, w, w,
+	  w, w, w, w, w, w, w, w
+	]
+	temp = sense.get_temperature()
+	if temp >= 20:
+	    sense.set_pixels(hot)
+	else:
+	    sense.set_pixels(cold)
+	    
+	bc = (0, 0, 205)
+	tc = (255,165,0)
+	c = 0
 
-picture = [
-  b, b, b, b, b, b, b, b,
-  b, y, y, b, b, y, y, b, 
-  b, y, y, b, b, y, y, b,  
-  y, y, y, y, y, y, y, y, 
-  y, y, b, y, y, b, y, y, 
-  y, r, y, b, b, y, r, y, 
-  y, y, y, y, y, y, y, y, 
-  b, y, y, y, y, y, y, b,
-]
-sense.set_pixels(picture)
-sleep(2)
-sense.show_message("picachu!")
-temp = sense.get_temperature()
-sense.show_message( str(temp) )
+	# william and storm
 
-# peterzac scooby tiga
+	sense.show_message("hello I like science!!!!")
+	b = (0,0,0)
+	y = (255,255,0)
+	r = (255,0,0)
 
-sense.show_message("The tempratue is", scroll_speed=0.01)
-temp = sense.get_temperature()
-sense.show_message( str(temp) )
-w = (255, 255, 255)
-y = (255, 255, 0)
-g = (0, 255, 0)
-b = (0, 0, 0)
-temp = sense.get_temperature()
-hot = [
-  b, b, b, b, b, y, y, b,
-  b, b, b, b, y, y, y, y,
-  b, b, b, b, b, y, y, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  b, b, b, b, b, b, b, b,
-  g, g, g, g, g, g, g, g,
-  g, g, g, g, g, g, g, g
-]
-cold = [
-  b, b, w, b, b, b, w, b,
-  b, b, b, b, b, w, b, b,
-  b, w, b, b, b, b, b, w,
-  b, b, b, b, w, b, b, b,
-  w, b, b, w, b, b, w, b,
-  b, b, b, b, b, b, b, b,
-  w, w, w, w, w, w, w, w,
-  w, w, w, w, w, w, w, w
-]
-temp = sense.get_temperature()
-if temp >= 20:
-    sense.set_pixels(hot)
-else:
-    sense.set_pixels(cold)
 
-sleep(2)
-sense.show_message("bye astronauts", scroll_speed=0.01)
-    
-picture = [
-  b, b, b, b, b, b, b, b,
-  b, b, w, w, w, w, b, b,
-  b, b, w, y, y, w, b, b,
-  b, b, w, y, y, w, b, b,
-  b, b, w, w, w, w, b, b,
-  w, w, w, w, w, w, w, w,
-  w, b, w, w, w, w, b, w,
-  b, b, w, b, b, w, b, b, 
-]
-sense.set_pixels(picture)
 
-# arun adam
+	picture = [
+	  b, b, b, b, b, b, b, b,
+	  b, y, y, b, b, y, y, b, 
+	  b, y, y, b, b, y, y, b,  
+	  y, y, y, y, y, y, y, y, 
+	  y, y, b, y, y, b, y, y, 
+	  y, r, y, b, b, y, r, y, 
+	  y, y, y, y, y, y, y, y, 
+	  b, y, y, y, y, y, y, b,
+	]
+	sense.set_pixels(picture)
+	sleep(2)
+	sense.show_message("picachu!")
+	temp = sense.get_temperature()
+	sense.show_message( str(temp) )
 
-green = (0,255,0)
-sense.show_message("This is for all the code clubs around the UK", scroll_speed=0.0, text_colour= green)
+	# peterzac scooby tiga
 
-w = (255,215,0)
-b = (255,0,255)
+	sense.show_message("The tempratue is", scroll_speed=0.01)
+	temp = sense.get_temperature()
+	sense.show_message( str(temp) )
+	w = (255, 255, 255)
+	y = (255, 255, 0)
+	g = (0, 255, 0)
+	b = (0, 0, 0)
+	temp = sense.get_temperature()
+	hot = [
+	  b, b, b, b, b, y, y, b,
+	  b, b, b, b, y, y, y, y,
+	  b, b, b, b, b, y, y, b,
+	  b, b, b, b, b, b, b, b,
+	  b, b, b, b, b, b, b, b,
+	  b, b, b, b, b, b, b, b,
+	  g, g, g, g, g, g, g, g,
+	  g, g, g, g, g, g, g, g
+	]
+	cold = [
+	  b, b, w, b, b, b, w, b,
+	  b, b, b, b, b, w, b, b,
+	  b, w, b, b, b, b, b, w,
+	  b, b, b, b, w, b, b, b,
+	  w, b, b, w, b, b, w, b,
+	  b, b, b, b, b, b, b, b,
+	  w, w, w, w, w, w, w, w,
+	  w, w, w, w, w, w, w, w
+	]
+	temp = sense.get_temperature()
+	if temp >= 20:
+	    sense.set_pixels(hot)
+	else:
+	    sense.set_pixels(cold)
 
-picture = [
-    w, w, w, w, w, w, w, w,
-    w, w, b, w, w, b, w, w,
-    w, w, w, w, w, w, w, w,
-    b, w, w, b, b, w, w, b,
-    b, b, w, w, w, w, b, b,
-    w, b, b, w, w, b, b, w,
-    w, w, b, w, w, b, w, w,
-    w, w, b, b, b, b, w, w,
-]
-sense.set_pixels(picture)
-sleep(2)
-temp = round( sense.get_temperature(), 1 )
-sense.show_message( str(temp) )
+	sleep(2)
+	sense.show_message("bye astronauts", scroll_speed=0.01)
+	    
+	picture = [
+	  b, b, b, b, b, b, b, b,
+	  b, b, w, w, w, w, b, b,
+	  b, b, w, y, y, w, b, b,
+	  b, b, w, y, y, w, b, b,
+	  b, b, w, w, w, w, b, b,
+	  w, w, w, w, w, w, w, w,
+	  w, b, w, w, w, w, b, w,
+	  b, b, w, b, b, w, b, b, 
+	]
+	sense.set_pixels(picture)
 
-# adam arun
-temp= sense.get_temperature()
-blue= (0,206,209)
-sense.show_message("space",text_colour=blue, scroll_speed=0.1)
+	# arun adam
 
-w = (255, 255, 255)
-b = (0, 0, 0)
+	green = (0,255,0)
+	sense.show_message("This is for all the code clubs around the UK", scroll_speed=0.0, text_colour= green)
 
-picture = [
-    b, b, b,w, w, w, b, b,
-    b, b, w, b, w, b, w, b,
-    b, b, w, w, w, w, w, b,
-    b, b, w, b, b, b, w, b,
-    b, b, w, b, b, b, w, b,
-    b, b, w, b, b, b, w, b,
-    b, b, b, w, b, w, b, b,
-    b, b, b, b, w, b, b, b
-]
+	w = (255,215,0)
+	b = (255,0,255)
+	picture = [
+	    w, w, w, w, w, w, w, w,
+	    w, w, b, w, w, b, w, w,
+	    w, w, w, w, w, w, w, w,
+	    b, w, w, b, b, w, w, b,
+	    b, b, w, w, w, w, b, b,
+	    w, b, b, w, w, b, b, w,
+	    w, w, b, w, w, b, w, w,
+	    w, w, b, b, b, b, w, w,
+	]
+	sense.set_pixels(picture)
+	sleep(2)
+	temp = round( sense.get_temperature(), 1 )
+	sense.show_message( str(temp) )
+	blue = (0,255,255)
+	sense.show_message('degrees', scroll_speed=0.0, text_colour = blue)
 
-sense.set_pixels(picture)
+	# adam arun
+	temp= sense.get_temperature()
+	blue= (0,206,209)
+	sense.show_message("space",text_colour=blue, scroll_speed=0.1)
+
+	w = (255, 255, 255)
+	b = (0, 0, 0)
+
+	picture = [
+	    b, b, b,w, w, w, b, b,
+	    b, b, w, b, w, b, w, b,
+	    b, b, w, w, w, w, w, b,
+	    b, b, w, b, b, b, w, b,
+	    b, b, w, b, b, b, w, b,
+	    b, b, w, b, b, b, w, b,
+	    b, b, b, w, b, w, b, b,
+	    b, b, b, b, w, b, b, b
+	]
+
+	sense.set_pixels(picture)
 
